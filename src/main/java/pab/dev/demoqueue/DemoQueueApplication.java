51c88a5f69da49ed5a79327b6261182919392af0
pab_thinkpad <pablo93bio@gmail.com>
2024-06-04 16:18:17 +0200

    project init

---
+++ src/main/java/pab/dev/demoqueue/DemoQueueApplication.java
@@ -0,0 +1,13 @@
+package pab.dev.demoqueue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoQueueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoQueueApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pab/dev/demoqueue/DemoQueueApplicationTests.java
@@ -0,0 +1,13 @@
+package pab.dev.demoqueue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoQueueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
